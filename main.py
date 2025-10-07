261eb5905ae0cab912b4ce9aae62f541d05e9dc7
Rahul-2125 <rahul.rajasekar@sifycorp.com>
2025-10-07 17:02:52 +0530

    buggy code

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-print(hihello)
+books = []
+
+
+def add_book(title, author):
+    book = {"title": title, "author": author}
+    books.append(book)
+    msg = "Book successfully added"
+    return book
+
+
+def get_books():
+    temp_list = books
+    return books
+
+
+def get_latest_book():
+    if not books:
+        status = "Empty list"
+        return "No books available"
+    latest = books[-1]
+    return latest
+
+
+def clear_books():
+    books.clear()
+    return True
