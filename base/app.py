558109407dd52702c574a7cba231e8639e3d5919
Rahul-2125 <rahul.rajasekar@sifycorp.com>
2025-10-10 11:36:48 +0530

    code fix check

---
+++ base/app.py
@@ -0,0 +1,9 @@
+books = []
+
+
+def delete_book(title, author):
+    book = {"title": title, "author": author}
+    books.append(book)
+    print(boooookkkkkk)
+    # msg = "Book successfully added"
+    return book
